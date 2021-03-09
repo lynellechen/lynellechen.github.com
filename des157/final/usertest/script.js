bbbb8a30b9b7b0b2cdb7f7a9c2a0d62105d5bd35
Lynelle Chen <leychen@ucdavis.edu>
2021-03-09 12:04:57 -0800

    added final user test

--- des157/final/usertest/script.js
+++ des157/final/usertest/script.js
@@ -17,6 +17,9 @@
 
     let currentImage = 0;
 
+    // usertest tasks
+    alert("Welcome! Please test the following:\n1. Hover over the images on the home page before clicking on them\n2. Click on the images inside the pages.\n3. Test the navigation links inside the pages.");
+
     // applied to each image on the home page
     for (const eachImg of collection) {
         eachImg.addEventListener('mouseover', function() {
